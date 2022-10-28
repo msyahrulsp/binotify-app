827c7781d7143490caf13ae3d14310d98a543787
msyahrulsp <13520161@std.stei.itb.ac.id>
2022-10-28 14:46:59 +0700

    fea: add lagu

--- database/binotify.sql
+++ database/binotify.sql
@@ -11,8 +11,8 @@ CREATE TABLE `binotify`.`album` (
   `genre` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`album_id`)
 );
-INSERT INTO `binotify`.`album` VALUES (1,'The Best of Me','Ardhito Pramono',749,'./assets/images/album/1.jpg','2010-01-01','Pop');
-INSERT INTO `binotify`.`album` VALUES (2,'The Worst of Me','Badlife',935,'./assets/images/album/2.jpg','2010-01-01','Pop');
+INSERT INTO `binotify`.`album` VALUES (1,'The Best of Me','Ardhito Pramono',919,'./assets/images/album/1.jpg','2010-01-01','Pop');
+INSERT INTO `binotify`.`album` VALUES (2,'The Worst of Me','Badlife',1267,'./assets/images/album/2.jpg','2010-01-01','Pop');
 
 DROP TABLE IF EXISTS `binotify`.`song`;
 CREATE TABLE `binotify`.`song` (
@@ -34,11 +34,14 @@ INSERT INTO `binotify`.`song` VALUES
 (2,'Goosebumps','Ardhito Pramono', '2000-10-11','sedih',164,'./assets/musics/song2.mp3','./assets/images/image2.png',1),
 (3,'Semoga, Ya', 'Ardhito Pramono','2000-10-12','sedih',268,'./assets/musics/song3.mp3','./assets/images/image3.png',1),
 (4,'cigarretes of ours','Ardhito Pramono', '2000-10-13','sedih',230,'./assets/musics/song4.mp3','./assets/images/image4.png',1),
-(5,'Sesuatu Di Jogja','Badlife', '2000-10-14','sedih',307,'./assets/musics/song5.mp3','./assets/images/image5.png',2),
+(5,'Sesuatu Di Jogja','Badlife', '2000-10-14','sedih',287,'./assets/musics/song5.mp3','./assets/images/image5.png',2),
 (6,'Rehat','Badlife','2000-10-15','sedih',361,'./assets/musics/song6.mp3','./assets/images/image6.png',2),
 (7,'Untuk Perempuan Yang Sedang Di Pelukan', 'Badlife','2000-10-10','sedih',342,'./assets/musics/song7.mp3','./assets/images/image7.png',2),
 (8,'Langit Abu-Abu','Badlife', '2000-10-11','sedih',218,'./assets/musics/song8.mp3','./assets/images/image8.png',2),
-(9,'HAHA HIHI','Badlife', '2000-10-11','sedih',257,'./assets/musics/song9.mp3','./assets/images/image9.jpg',NULL);
+(9,'HAHA HIHI','Badlife', '2000-10-11','sedih',257,'./assets/musics/song9.mp3','./assets/images/image9.jpg',NULL),
+(10, 'Tracing That Dream','Yoasobi','2020-10-18','Pop',239,'./assets/musics/song10.mp3','./assets/images/image10.jpeg',NULL),
+(11, 'Fortune Cookie','AKB48','2020-10-19','Pop',322,'./assets/musics/song11.mp3','./assets/images/image11.jpeg', NULL),
+(12, 'Stella-Rium','Kano','2020-10-20','Pop',244,'./assets/musics/song12.mp3','./assets/images/image12.jpeg', NULL);
 
 DROP TABLE IF EXISTS `binotify`.`user`;
 CREATE TABLE `binotify`.`user` (
